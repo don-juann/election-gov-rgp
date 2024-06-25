db74979a3e1db8df11678b18afed869e32ccdee0
don-juann <221122@astanait.edu.kz>
2024-06-25 15:24:29 +0500

    changed logo, added flag of KZ near logo, removed redundant files, improved adaptiveness.

--- js/scripts.js
+++ js/scripts.js
@@ -16,31 +16,49 @@ function scrollToTop() {
     });
 }
 
+// Адаптивность
+function handleViewportWidth() {
+    const headerTitle = document.getElementById('headerTitle');
+    const viewportWidth = window.innerWidth;
+
+    const thresholdWidth = 900; 
+
+    if (viewportWidth < thresholdWidth) {
+        headerTitle.style.display = 'none';
+    } else {
+        headerTitle.style.display = 'block';
+    }
+}
+
+window.addEventListener('resize', handleViewportWidth);
+window.addEventListener('DOMContentLoaded', handleViewportWidth);
+
+
 // Анимация текста
-document.addEventListener('DOMContentLoaded', function() {
-     const headings = document.querySelectorAll('.fancy h1');
+// document.addEventListener('DOMContentLoaded', function() {
+//      const headings = document.querySelectorAll('.fancy h1');
      
-     headings.forEach((heading, index) => {
-         let text = heading.textContent.trim();
-         heading.textContent = '';
+//      headings.forEach((heading, index) => {
+//          let text = heading.textContent.trim();
+//          heading.textContent = '';
  
-         for (let i = 0; i < text.length; i++) {
-             const span = document.createElement('span');
-             span.textContent = text[i];
-             heading.appendChild(span);
-         }
-     });
+//          for (let i = 0; i < text.length; i++) {
+//              const span = document.createElement('span');
+//              span.textContent = text[i];
+//              heading.appendChild(span);
+//          }
+//      });
  
-     function fadeInHeading() {
-         headings.forEach((heading, index) => {
-             const spans = heading.querySelectorAll('span');
-             spans.forEach((span, index) => {
-                 setTimeout(() => {
-                     span.style.opacity = 1;
-                 }, index * 50); 
-             });
-         });
-     }
+//      function fadeInHeading() {
+//          headings.forEach((heading, index) => {
+//              const spans = heading.querySelectorAll('span');
+//              spans.forEach((span, index) => {
+//                  setTimeout(() => {
+//                      span.style.opacity = 1;
+//                  }, index * 50); 
+//              });
+//          });
+//      }
  
-     setTimeout(fadeInHeading, 500);
- });
+//      setTimeout(fadeInHeading, 500);
+//  });
--- js/table.js
+++
@@ -1,1 +0,0 @@
-document.getElementById('list-item-1').click();
